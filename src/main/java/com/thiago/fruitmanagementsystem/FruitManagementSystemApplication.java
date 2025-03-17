4075c9fee10188cd9ee48bcfab4e6ff577b7f872
thiagovb62 <thiagovb62@gmail.com>
2025-03-17 20:07:32 -0300

    initialize Fruit Management System with Spring Boot and PostgresSQL setup

---
+++ src/main/java/com/thiago/fruitmanagementsystem/FruitManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.fruitmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitManagementSystemApplication.class, args);
+    }
+
+}
